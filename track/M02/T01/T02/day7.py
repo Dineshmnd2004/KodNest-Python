2cc71d2e57cf1102fe5e5dff2da5ea9b18282361
Naga Dinesh Mesaragandla <mndinesh379@gmail.com>
2026-08-20 16:05:40 +0530

    day7

---
+++ track/M02/T01/T02/day7.py
@@ -0,0 +1,21 @@
+class Student:
+
+    def __init__(self, roll, name):
+        if roll  > 0:
+            self.__roll = roll
+        else:
+            self.__roll = None
+            print("Invalid Roll No")
+        self.__name = name
+
+    @property
+    def roll(self):
+        return self.__roll
+
+    @property
+    def name(self):
+        return self.__name
+
+s1 = Student(12, "Arun")
+print(s1.roll)
+print(s1.name)
